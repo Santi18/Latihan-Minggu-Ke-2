ab68f235a15e58d60184e206eceb416cdd9542cc
Santi18 <santinoviani97@gmail.com>
2017-03-14 21:04:37 +0700

    Add files via upload

---
+++ belajar3.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#define PHI 3.14//Konstanta
+
+  using namespace std;
+  int main()
+  {
+  float jari_jari,luas;
+  //Konstanta 
+  jari_jari=25;
+   luas = 0.5 * PHI * jari_jari * jari_jari;
+   cout<<"Data Lingkaran";
+   cout<<"nJari-jari:"<<jari_jari;
+   cout<<"nLuas:"<<luas;
+   cout<<"\n";
+   return 0;
+   }
---
+++ belajar4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+  using namespace std;
+  
+   int main()
+   {
+   cout<<"Ukuran Char :"<<sizeof(char) <<endl;
+   cout<<"Ukuran int :"<<sizeof(int) <<endl;
+   cout<<"Ukuran short int :"<<sizeof(short int) <<endl;
+   cout<<"Ukuran long int :"<<sizeof(long in) <<endl;
+   cout<<"Ukuran float :"<<sizeof(float) <<endl;
+   cout<<"Ukuran double :"<<sizeof(double) <<endl;
+   cout<<"Ukuran wchar_t :"<<sizeof(wchar_t) <<endl;
+   return 0;
+   }
+   
